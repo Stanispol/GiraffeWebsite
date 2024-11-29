c06db2f76fdc9fb96f9ccec19e8aa5d7db08f43d
Stanispol <stas.pol.05@yandex.com>
2024-11-29 12:14:54 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("open-popup-btn1").addEventListener("click", function() {
+    document.getElementById("my-popup").classList.add("open")
+})
+
+document.getElementById("close-my-popup-btn").addEventListener("click", function() {
+    document.getElementById("my-popup").classList.remove("open")
+})
+
+document.getElementById("open-popup-btn2").addEventListener("click", function() {
+    document.getElementById("my-popup").classList.add("open")
+})
+
+document.getElementById("open-popup-btn3").addEventListener("click", function() {
+    document.getElementById("my-popup").classList.add("open")
+})
